c810a0d80cfe54f861376a6627331a6cb1987f84
Abdalla_Shaban <124538890+Abdalla-Shaban@users.noreply.github.com>
2023-08-21 01:52:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-import Headroom from "/node_modules/headroom.js/dist/headroom.js";
-let header = document.querySelector("header");
-let headRoom = new Headroom(header);
-headRoom.init();
+
 let fixdNav = document.querySelector(".fixed-nav");
 
 let bullets = document.querySelectorAll(".bullet");
@@ -204,4 +201,7 @@ function getCustomStorage(key, value) {
   }
 }
 
-
+import Headroom from "/node_modules/headroom.js/dist/headroom.js";
+let header = document.querySelector("header");
+let headRoom = new Headroom(header);
+headRoom.init();
